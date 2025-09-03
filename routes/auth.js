7347b140ab29d20c969d39fb3ec2e50f86664a3c
Nikhil Saxena <147965031+Nikhil2253@users.noreply.github.com>
2025-09-03 20:11:19 +0530

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+import {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+  logout,
+} from "../controllers/authController.js";
+
+async function routes(fastify, opts) {
+  fastify.post("/register", register);
+  fastify.post("/login", login);
+  fastify.post("/forgot-password", forgotPassword);
+  fastify.post("/reset-password/:token", resetPassword);
+  fastify.post("/logout", { preHandler: [fastify.authenticate] }, logout);
+}
+
+export default routes;
