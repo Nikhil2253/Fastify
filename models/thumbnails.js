fc369c235003bd4a16dcafcd48d87ea8a067a195
Nikhil Saxena <147965031+Nikhil2253@users.noreply.github.com>
2025-09-03 20:10:19 +0530

    Create thumbnails.js

---
+++ models/thumbnails.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const thumbnailSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  videoName: {
+    type: String,
+    required: true,
+  },
+  version:{
+    type:String,
+  },
+  image:{
+    type:String,
+    required:true,
+  },
+  paid:{
+    type:Boolean,
+    default:false
+  },
+});
+
+const Thumbnail = mongoose.model("Thumbnail", thumbnailSchema);
+
+export default Thumbnail;
