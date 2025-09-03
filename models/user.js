28e6c20de95c91d6a571f94ab1404bbfd0fca918
Nikhil Saxena <147965031+Nikhil2253@users.noreply.github.com>
2025-09-03 20:10:39 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password:{
+    type:String,
+    required:true,
+  },
+  country:{
+    type:String,
+  },
+  resetPasswordToken:{
+    type:String,
+  },
+  resetPasswordExpiry:{
+    type:Date,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
